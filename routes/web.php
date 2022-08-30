bef2c3a542a278f36e7ad25bdb59a862d17a19f0
TiagoDat1012 <atendimento@wssh.com.br>
2022-08-30 15:11:00 -0300

    Definição de parametros padrão

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
+/// *** INICIAL
 Route::get('/','InicialController@inicial');
-Route::get('/Login','LoginController@Login');
+
+/// *** CADASTRO DE USUARIOS
 Route::get('/CadastroUsuarios','CadastroUsuariosController@CadastroUsuarios');
 
-Route::get('/Login/{email}/{senha}', 
-    function($email,$senha){
+/// *** LOGIN
+Route::get('/Login','LoginController@Login');
+/// *** OS PAREMETROS OPCIONAIS DEVEM SER DEFINIDOS DA DIREITA PATA ESQUERDA <<<<<
+Route::get('/Login/{email?}/{senha?}', 
+    function($email = 'Email não informado',$senha = 'Senha não informada'){
     echo 'Email: '. $email . '<br>';
     echo 'Senha: '. $senha . '<br>';
 });
