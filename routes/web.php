25d57113b350179d674c0b375f9d7bc17aec6de4
TiagoDat1012 <atendimento@wssh.com.br>
2022-08-30 15:01:52 -0300

    Passar paratros pelas rotas

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,10 @@ Route::get('/','InicialController@inicial');
 Route::get('/Login','LoginController@Login');
 Route::get('/CadastroUsuarios','CadastroUsuariosController@CadastroUsuarios');
 
+Route::get('/Login/{email}/{senha}', 
+    function($email,$senha){
+    echo 'Email: '. $email . '<br>';
+    echo 'Senha: '. $senha . '<br>';
+});
+
  
