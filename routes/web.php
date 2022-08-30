6257088cd315e408f34893d5a5424a5ffaa1f387
TiagoDat1012 <atendimento@wssh.com.br>
2022-08-30 15:36:19 -0300

    Rotas com expressões regulares

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,22 @@
 <?php
 
-/// *** INICIAL
+/// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| *** INICIAL
 Route::get('/','InicialController@inicial');
 
-/// *** CADASTRO DE USUARIOS
+/// ||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| *** CADASTRO DE USUARIOS
 Route::get('/CadastroUsuarios','CadastroUsuariosController@CadastroUsuarios');
+Route::get('/CadastroUsuarios/{id_usuario?}', 
+    function($id_usuario = 0){
+        echo 'Senha: '. $id_usuario . '<br>';
+})->where('id_usuario','[0-9]+');
 
-/// *** LOGIN
+/// |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||| *** LOGIN 
 Route::get('/Login','LoginController@Login');
 /// *** OS PAREMETROS OPCIONAIS DEVEM SER DEFINIDOS DA DIREITA PATA ESQUERDA <<<<<
-Route::get('/Login/{email?}/{senha?}', 
-    function($email = 'Email não informado',$senha = 'Senha não informada'){
-    echo 'Email: '. $email . '<br>';
-    echo 'Senha: '. $senha . '<br>';
-});
+Route::get('/Login/{email?}/{id_usuario?}', 
+    function($email = 'Email não informado' , $id_usuario = 0){
+        echo 'Email: '. $email . '<br>';
+        echo 'Senha: '. $id_usuario . '<br>';
+})->where('id_usuario','[0-9]+')->where('email','[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$');
 
  
