706ea0c1e9eca63dad84d90e2518ee073c0104ab
TiagoDat1012 <atendimento@wssh.com.br>
2022-09-03 20:00:47 -0300

    41. Encaminhando parâmetros do controlador para visualização

--- app/Http/Controllers/TesteController.php
+++ app/Http/Controllers/TesteController.php
@@ -9,6 +9,8 @@ class TesteController extends Controller
     public function teste( $p1 , $p2 ){
         $p11 = intval($p1);
         $p12 = intval($p2);        
-        echo "A soma de $p1 + $p2 é :".($p1+$p2);
+        //echo "A soma de $p1 + $p2 é :".($p1+$p2);
+
+        return view('site.teste', ['p1' => $p1 , 'p2' => $p2] );
     }
 }
