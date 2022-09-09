2ce4a306db520ced2075506877328778c0ba1947
TiagoDat1012 <atendimento@wssh.com.br>
2022-09-09 17:03:13 -0300

    PARTIALS

--- resources/views/site/inicial.blade.php
+++ resources/views/site/inicial.blade.php
@@ -2,4 +2,8 @@
 @section('titulo' , $titulo['titulo']) {{-- PASSAR PARAMETRO --}}
 @section('body')
     inicio
+
+    <h1>Partials</h1>
+    @include('site.layouts._partials.teste') {{-- PARTIALS, NÃO PRECISA COLOCAR "blade.php" --}}
+
 @endsection
---
+++ resources/views/site/layouts/_partials/teste.blade.php
@@ -0,0 +1,1 @@
+<b>Partials</b>
