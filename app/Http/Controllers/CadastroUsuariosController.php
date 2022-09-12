748ec80b767f565e5a8f0460d8b4322ee2fdd824
TiagoDat1012 <atendimento@wssh.com.br>
2022-09-12 10:33:23 -0300

    Envio de Formulario via

--- app/Http/Controllers/CadastroUsuariosController.php
+++ app/Http/Controllers/CadastroUsuariosController.php
@@ -8,8 +8,54 @@ class CadastroUsuariosController extends Controller
 {
     public function CadastroUsuarios(){
 
-        $matriz = ['titulo' => 'Usuarios como Matriz'];
+        $aviso  = "";
+        $erro   = 1;
+        $matriz = [ 'titulo' => 'Usuarios como Matriz' , 'aviso' => $aviso , 'erro' => $erro ];
 
-        return view('site.cadastroUsuarios' , ['titulo' => $matriz]);
+        // SUPER GLOBAL GET
+        if(isset($_GET["nome"])){
+            $erro  = 0;
+            $nome  = $_GET["nome"];
+            $email = $_GET["email"];
+            $senha = $_GET["senha"];
+
+            /// *** REGRAS DE NEGÓCIO
+            if($nome == '' || $nome == null){
+                $erro++;
+                $aviso .= " Preencha o Nome; ";
+            }elseif ($email == '' || $email == null) {
+                $erro++;
+                $aviso .= " Preencha o E-mail; ";
+            }elseif ($senha == '' || $senha == null) {
+                $erro++;
+                $aviso .= " Preencha a Senha; ";
+            }else{
+                
+                $nomeEx = explode(" " , $nome);
+                if(count($nomeEx) <= 1){
+                    $erro++;
+                    $aviso .= " Formato do [Nome] incorreto; ";
+                }
+
+                /// *** APLICA REGEX NO EMAIL
+                $pattern = "/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/";
+                if(preg_match($pattern, $email) == 0){
+                    $erro++;
+                    $aviso .= " E-mail inválido; ";
+                }
+
+            }
+
+            /// *** FINALIZA REGRAS DE NEGÓCIO
+        }
+        
+        
+        if ($erro == 0) {
+            return redirect()->route('resp.sucesso'); // REDIRECIONAMENTO DE ROTA
+        }else{
+            $matriz = [ 'titulo' => 'Usuarios como Matriz' , 'aviso' => $aviso ];
+            return view('site.cadastroUsuarios' , ['titulo' => $matriz]);
+        }
+
     }
 }
--- resources/views/site/cadastroUsuarios.blade.php
+++ resources/views/site/cadastroUsuarios.blade.php
@@ -1,5 +1,59 @@
 @extends('site.layouts.body')
 @section('titulo' , $titulo['titulo']) {{-- PASSAR PARAMETRO --}}
 @section('body')
-    <h1>Cadastro de Usuários</h1>
+
+<!-- Page header -->
+    <div class="page-header page-header-default">
+        <div class="page-header-content">
+            <div class="page-title">
+                <h4>Cadastro de Usuários</h4>
+                {{ $titulo["aviso"] }}
+            </div>
+
+        </div>
+
+        <div class="breadcrumb-line">
+            <ul class="breadcrumb">
+                <li><a href="/Home/Index"><i class="icon-home2 position-left"></i> Home</a></li>
+                <li class="active">Cadastro de Usuários</li>
+            </ul>
+        </div>
+    </div>
+<!-- /page header -->
+<!-- Basic layout-->
+
+        <div class="panel panel-flat">
+         
+
+            <div class="panel-body">
+               
+            <form action="{{ route('site.cadastro-de-clientes') }}">
+
+                <div class="form-group">
+                    <label>Name:</label>
+                    <input type="text" name="nome" id="nome" class="form-control" placeholder="Nome">
+                </div>
+                
+                <div class="form-group">
+                    <label>E-mail:</label>
+                    <input type="text" name="email" id="email" class="form-control" placeholder="E-mail">
+                </div>
+
+                <div class="form-group">
+                    <label>Senha:</label>
+                    <input type="password" name="senha" id="senha" class="form-control">
+                </div>
+
+                <div class="text-right">
+                    <button type="submit" class="btn btn-primary">Salvar<i class="icon-arrow-right14 position-right"></i></button>
+                </div>
+
+            </form>
+
+            </div>
+        </div>
+<!-- /basic layout -->
+
+
+
 @endsection
